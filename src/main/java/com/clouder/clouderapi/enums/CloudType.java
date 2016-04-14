f80886be39832de301d49cc37644cba227017e68
Shriroop Joshi <shriroop.joshi@gmail.com>
2016-04-14 20:59:47 +0530

    Changed enums to String values

--- src/main/java/com/clouder/clouderapi/enums/CloudType.java
+++ src/main/java/com/clouder/clouderapi/enums/CloudType.java
@@ -2,6 +2,19 @@ package com.clouder.clouderapi.enums;
 
 public enum CloudType {
 
-	GOOGLEDRIVE, ONEDRIVE, DROPBOX;
+	GOOGLEDRIVE("GOOGLEDRIVE"),
+	ONEDRIVE("ONEDRIVE"),
+	DROPBOX("DROPBOX")
+	;
 
+	private String VALUE;
+	
+	private CloudType(String Value) {
+		this.VALUE = Value;
+	}
+	
+	@Override
+	public String toString() {
+		return this.VALUE;
+	}
 }

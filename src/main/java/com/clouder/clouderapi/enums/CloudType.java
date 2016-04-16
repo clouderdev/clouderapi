f28989bdee19edb2185f2f66e107e534863e5fdf
Shrinivas Shukla <ssshukla1993@gmail.com>
2016-04-17 00:02:19 +0530

    Committing updated CloudType

--- src/main/java/com/clouder/clouderapi/enums/CloudType.java
+++ src/main/java/com/clouder/clouderapi/enums/CloudType.java
@@ -2,23 +2,16 @@ package com.clouder.clouderapi.enums;
 
 public enum CloudType {
 
-	GOOGLEDRIVE("GOOGLEDRIVE"),
-	ONEDRIVE("ONEDRIVE"),
-	DROPBOX("DROPBOX")
-	;
+	GOOGLEDRIVE("GOOGLEDRIVE"), ONEDRIVE("ONEDRIVE"), DROPBOX("DROPBOX");
 
-<<<<<<< HEAD
-}
-=======
 	private String VALUE;
-	
+
 	private CloudType(String Value) {
 		this.VALUE = Value;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.VALUE;
 	}
 }
->>>>>>> f80886be39832de301d49cc37644cba227017e68
